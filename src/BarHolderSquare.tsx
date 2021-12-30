0886097e4a43e3ce77e88604042722e05ac8ca27
Anwesh43 <anweshthecool0@gmail.com>
2021-12-30 17:43:37 +0530

    adding BarHolderSquare component

---
+++ src/BarHolderSquare.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useStyle} from './hooks'
+
+interface BHSProps {
+    w : number, h : number, scale : number, onClick : Function
+}
+
+const BarHolderSquare = (props : BHSProps) => {
+    const {squareStyle, barStyle} = useStyle(props.w, props.h, props.scale)
+    return (
+        <React.Fragment>
+            <div style = {barStyle()}></div>
+            {[0, 1].map((i) => (<div key = {`square_${i}`} style = {squareStyle(i)}></div>))}
+        </React.Fragment>        
+    )
+}
+
+export default BarHolderSquare
